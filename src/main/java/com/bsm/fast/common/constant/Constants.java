ffc6c0b022269688fb57b9960e74ecd7b46ce3b4
limingxin <limingxin@jiangtai.com>
2020-07-18 14:07:56 +0800

    新增消息相应实体、swagger配置

---
+++ src/main/java/com/bsm/fast/common/constant/Constants.java
@@ -0,0 +1,20 @@
+package com.bsm.fast.common.constant;
+
+/**
+ * 通用常量信息
+ * 
+ * @author limingxin
+ */
+public class Constants {
+
+    /**
+     * 成功标记
+     */
+    public static final Integer SUCCESS = 200;
+    /**
+     * 失败标记
+     */
+    public static final Integer FAIL = 501;
+
+
+}
---
+++ src/main/java/com/bsm/fast/config/SwaggerConfig.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package com.bsm.fast.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            .select()
+            //加了ApiOperation注解的类，才生成接口文档
+            .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+            .paths(PathSelectors.any())
+            .build()
+            .securitySchemes(security());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("快速开发框架")
+            .description("fast文档")
+            .termsOfServiceUrl("")
+            .version("2.0.0")
+            .build();
+    }
+
+    private List<ApiKey> security() {
+        return newArrayList(
+            new ApiKey("token", "token", "header")
+        );
+    }
+
+}
